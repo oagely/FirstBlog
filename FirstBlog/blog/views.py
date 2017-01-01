c0989e9c09414b31a135aa1ddd9d779e5bafb187
Omar Agely <oagely@gmail.com>
2017-01-01 14:14:30 -0500

    django!
    website in django

---
+++ FirstBlog/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render_to_response
+
+from blog.models import posts
+
+def home(request):
+	entries = posts.objects.all()[:10]
+
+	return render_to_response('index.html', {'posts': entries})
+
+# Create your views here.
